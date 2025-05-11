42c55bc27a329470b1d79439e3dc4785ec338ae4
Arslonbek <roziboyevarslon0110@gmail.com>
2025-05-11 18:12:52 +0500

    insert binary search tree session

---
+++ binary-search-tree/insert.js
@@ -0,0 +1,39 @@
+class Node {
+    constructor(value){
+        this.value = value
+        this.left = null
+        this.right = null
+    }
+}
+
+class BinarySearchTree {
+    constructor(){
+        this.root = null
+    }
+    insert(value){
+        const newNode = new Node(value)
+        if(this.root === null){
+            this.root = newNode
+        }else {
+            let current = this.root
+            while(true){
+                if(value === current.value) return undefined;
+                if(value < current.value){
+                    if(current.left === null){
+                        current.left = newNode
+                        return this
+                    }else {
+                        current = current.left
+                    }
+                }else {
+                    if(current.right === null){
+                        current.right = newNode
+                        return this
+                    }else{
+                        current = current.right
+                    }
+                }
+            }
+        }
+    }
+}
