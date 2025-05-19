13adaf5d656c8a2a4be7a8f51421566ba2b2f4f3
Arslonbek <roziboyevarslon0110@gmail.com>
2025-05-19 16:20:30 +0500

    add leetcode-3024

---
+++ leetcode-3024/main.js
@@ -0,0 +1,7 @@
+function triangleType (nums){
+    const [a, b, c] = nums
+    if(a + b <= c || a + c <= b || b + c <= a) return 'none'
+    if(a === b && b === c) return 'equilateral'
+    if(a === b || b === c || a === c) return 'isosceles'
+    return 'scalene'
+}
